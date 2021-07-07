950c8e28c0c9fbdc0bf26d5a55e384bf869b2fa2
jhamPac <cloud@kevia.me>
2021-07-06 19:55:27 -0700

    feat: which implementation

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jhampac/sysgo/pwd"
+)
+
+func main() {
+	pwd.Execute()
+}
---
+++ which/which.go
@@ -0,0 +1,55 @@
+package which
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Execute() {
+	optA := flag.Bool("a", false, "a")
+	optS := flag.Bool("s", false, "s")
+
+	flag.Parse()
+
+	flags := flag.Args()
+	if len(flags) == 0 {
+		fmt.Println("Please provide arguments")
+		os.Exit(1)
+	}
+
+	file := flags[0]
+	found := false
+
+	path := os.Getenv("PATH")
+	pathSlice := strings.Split(path, ":")
+	for _, dir := range pathSlice {
+		fullPath := dir + "/" + file
+
+		fileInfo, err := os.Stat(fullPath)
+		if err == nil {
+			mode := fileInfo.Mode()
+			if mode.IsRegular() {
+				if mode&0111 != 0 {
+					found = true
+					if *optS == true {
+						os.Exit(0)
+					}
+
+					if *optA == true {
+						fmt.Println(fullPath)
+					} else {
+						fmt.Println(fullPath)
+						os.Exit(0)
+					}
+				}
+			}
+		}
+	}
+
+	if found == false {
+		os.Exit(1)
+	}
+
+}
