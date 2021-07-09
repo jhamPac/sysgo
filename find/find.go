75d7af712f5f1a0405f4f6256c764d3722cf236f
jhamPac <cloud@kevia.me>
2021-07-08 18:19:24 -0700

    feat: set guards for flags being set or not

--- find/find.go
+++ find/find.go
@@ -4,36 +4,33 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
 )
 
-func walkFunction(path string, info os.FileInfo, err error) error {
-	fileInfo, e := os.Stat(path)
-	if e != nil {
-		return err
-	}
-
-	mode := fileInfo.Mode()
-	if mode.IsDir() || mode.IsRegular() {
-		fmt.Println(path)
-	}
-	return nil
-}
-
 func main() {
+	sFlag := flag.Bool("s", false, "Sockets")
+	pFlag := flag.Bool("p", false, "Pipes")
+	lkFlag := flag.Bool("lk", false, "Symbolic Links")
+	dFlag := flag.Bool("d", false, "Directories")
+	fFlag := flag.Bool("f", false, "Files")
+
 	flag.Parse()
 	flags := flag.Args()
 
+	printAll := false
+	if *sFlag && *pFlag && *lkFlag && *dFlag && *fFlag {
+		printAll = true
+	}
+
+	// all flags begin as flase
+	if !(*sFlag || *pFlag || *lkFlag || *dFlag || *fFlag) {
+		printAll = true
+	}
+
 	if len(flags) == 0 {
-		fmt.Println("More arguments needed.")
+		fmt.Println("Not enough arguments.")
 		os.Exit(1)
 	}
 
 	path := flags[0]
 
-	err := filepath.Walk(path, walkFunction)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 }
