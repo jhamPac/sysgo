dda850f4f30c8098fe79fc1fe4d9714ccddf2be7
jhamPac <cloud@kevia.me>
2021-07-08 18:11:04 -0700

    feat: implement basic find cli

--- find/find.go
+++ find/find.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func walkFunction(path string, info os.FileInfo, err error) error {
-	fileInfo, err := os.Stat(path)
-	if err != nil {
+	fileInfo, e := os.Stat(path)
+	if e != nil {
 		return err
 	}
 
@@ -19,5 +21,19 @@ func walkFunction(path string, info os.FileInfo, err error) error {
 }
 
 func main() {
+	flag.Parse()
+	flags := flag.Args()
 
+	if len(flags) == 0 {
+		fmt.Println("More arguments needed.")
+		os.Exit(1)
+	}
+
+	path := flags[0]
+
+	err := filepath.Walk(path, walkFunction)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
