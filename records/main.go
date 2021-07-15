7f490f3bd0f78ecc0f4518542b553e83b31c772d
jhamPac <cloud@kevia.me>
2021-07-15 08:32:28 -0700

    feat: csv creator

--- records/main.go
+++ records/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
 )
@@ -24,4 +25,39 @@ func main() {
 		os.Exit(-1)
 	}
 	defer output.Close()
+
+	inputData := [][]string{
+		{"Red", "Blue", "Green"},
+		{"White", "Yellow", "Orange"},
+		{"Purple", "Taupe", "Brown"},
+	}
+
+	writer := csv.NewWriter(output)
+	for _, record := range inputData {
+		err := writer.Write(record)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+	}
+	writer.Flush()
+
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = -1
+	allRecords, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, rec := range allRecords {
+		fmt.Printf("%s:%s:%s\n", rec[0], rec[1], rec[2])
+	}
 }
